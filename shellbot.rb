1732d7b5d0d2aa74c6b880c01121699f7cdd7851
CailinPitt <cailinpitt1@gmail.com>
2016-07-02 17:03:41 -0400

    added changes to readme

--- shellbot.rb
+++ shellbot.rb
@@ -1,8 +1,8 @@
 require 'slack-ruby-bot'
 
 class ShellBot < SlackRubyBot::Bot
-  match /^# (?<command>\w*\s*)$/ do |client, data, match|
-	result = `#{match[:command]}`
+  match /^\$ (?<command>.*)$/ do |client, data, match|
+		result = `#{match[:command]}`
     client.say(channel: data.channel, text: result)
   end
 end
